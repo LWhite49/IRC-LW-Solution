aeb6db2d0b60bfb40011b5f315fb4e13b0fcad71
LWhite49 <LWhite4965@outlook.com>
2023-08-01 15:49:32 -0400

    Added second screen

--- scripts/interactive-rating-component.js
+++ scripts/interactive-rating-component.js
@@ -16,3 +16,28 @@ document.querySelectorAll(`.input-button`).forEach( (button, index) => {
     /* Add function as a listener for clicking each button */
     button.addEventListener('click', selectListener);
 })
+
+/* Add listener to submit button that loads a new page if a value was selected */
+loadPageListener = () => {
+    /* Only load page if reviewScore has a value */
+    if (reviewScore) {
+        /* Create new page html */
+        let newHTML =
+        `<div class="response-graphic-wrap">
+            <img class="response-graphic" src="../images/illustration-thank-you.svg"/>
+        </div>
+        <div class="rating-review-wrap">
+            <button class="rating-review">You selected ${reviewScore} out of 5</button>
+        </div>
+        <div class="title-container-review">
+            <p class="review-title">Thank you!</p>
+        </div>
+        <div class="text-container-review">
+            <p class="review-text">We appreciate you taking the time to give a rating-
+            If you ever need more support. don't hesitate to
+            get in touch!</p>
+        </div>`
+        document.querySelector(`.main-container`).innerHTML = newHTML;
+    }
+}
+document.querySelector(`.submit-button`).addEventListener("click", loadPageListener);
